49e6f07392c40c01ecb8f33eff0c6ef352c50504
Fernando Alvarez <masterofer36@gmail.com>
2020-10-12 12:40:35 -0400

    feat: implement pass executor

--- src/stateTasks/executors/PassExecutor.ts
+++ src/stateTasks/executors/PassExecutor.ts
@@ -1,18 +1,39 @@
-import type { ChoiceStateDefinition } from '../../types/State';
-import type { StateExecutorOutput } from '../../types/StateExecutorOutput';
+import { StateProcessor } from '../../StateProcessor';
 import { StateTypeExecutor } from '../StateTypeExecutor';
+import type { StateExecutorOutput } from '../../types/StateExecutorOutput';
+import type { PassStateDefinition } from '../../types/State';
 
 export class PassExecutor implements StateTypeExecutor {
   public execute(
-    stateMachineName: string,
+    _stateMachineName: string,
     stateName: string,
-    definition: ChoiceStateDefinition,
+    definition: PassStateDefinition,
     json: string | undefined,
   ): Promise<StateExecutorOutput> {
+    console.log(`* * * Passed Task ${stateName} * * *`);
+    const input = this.processInput(json, definition);
+
     return Promise.resolve({
       Next: definition.Next,
       End: definition.End,
-      json: json || '{}',
+      json: this.processOutput(input, definition),
     });
+  }
+
+  private processInput(json: string | undefined, stateDefinition: PassStateDefinition): string {
+    const proccessedInputJson = StateProcessor.processInputPath(json, stateDefinition.InputPath);
+    // TODO: Parameters Task
+
+    return proccessedInputJson;
+  }
+
+  private processOutput(output: string, stateDefinition: PassStateDefinition): string {
+    let outputJson = output || '{}';
+
+    // TODO: Do Result Selector
+    outputJson = StateProcessor.processResultPath(outputJson, stateDefinition.ResultPath);
+    outputJson = StateProcessor.processOutputPath(outputJson, stateDefinition.OutputPath);
+
+    return outputJson;
   }
 }
