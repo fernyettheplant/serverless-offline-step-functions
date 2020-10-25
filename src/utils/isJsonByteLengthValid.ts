b1f9341e2b9ba8cef4e3b1ddd32d4c5b1b6acedd
Jesús Fernando Alvarez Franco <jefer_590@hotmail.com>
2020-10-25 14:21:10 -0400

    fix: byte length comparison

--- src/utils/isJsonByteLengthValid.ts
+++ src/utils/isJsonByteLengthValid.ts
@@ -4,5 +4,5 @@ export function isJsonByteLengthValid(json: string): boolean {
   const byteLength = Buffer.byteLength(json);
   const stepFunctionByteSizeLimit = 262144; // 256 Kb according to AWS
 
-  return byteLength === stepFunctionByteSizeLimit;
+  return byteLength <= stepFunctionByteSizeLimit;
 }
